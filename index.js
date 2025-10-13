688911cd184cda1745e76f9d8698531ac9b841ab
FearDread <ghaptonstall@gmail.com>
2025-10-12 19:46:33 -0700

    --fix agent start call

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const SecurityAgent = require("./src/agent");
+const colorizer = require('./src/utils/colorizer');
 // Start the agent
+if (require.main === module) {
 
   const agent = new SecurityAgent();
+  
   agent.start().catch(err => {
-    console.error('Fatal error:', err);
+    console.error(colorizer.error('Fatal error: ' + err.message));
     process.exit(1);
   });
+}
--- src/agent.js
+++ src/agent.js
@@ -341,13 +341,4 @@ SecurityAgent.prototype = {
   }
 };
 
-// Start the agent
-if (require.main === module) {
-  const agent = new SecurityAgent();
-  agent.start().catch(err => {
-    console.error(colorizer.error('Fatal error: ' + err.message));
-    process.exit(1);
-  });
-}
-
 module.exports = SecurityAgent;
